3f4d2c011012bf2348918d8d317f8a68f65bc585
FearlessJeny <161617854+FearlessJeny@users.noreply.github.com>
2025-07-04 22:23:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+$(function() {
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar").addClass("active");
+        } else {
+            $(".navbar").removeClass("active");
+        };
+    });
+    });
+
+
+    
